769838b22a8239df87005dc7ad90cea34027d5b2
feliclaria <felipe.claria@mi.unc.edu.ar>
2023-05-11 02:40:07 -0300

    p5e{3,5-8}

--- src/continuous.py
+++ src/continuous.py
@@ -1,5 +1,79 @@
 from random import random
+from typing import *
+import discrete as disc
 import math
 
-def exponential(lambd):
+def exponential(lambd: float) -> float:
+  """Distribución exponencial.
+
+  Parametros
+  ----------
+  lambd : float
+    `1.0` dividido por la media deseada. Debe ser distinto de cero.
+
+  Retorna
+  -------
+  float
+    Valor entre 0 e infinito si `lambd > 0`, y entre infinito negativo y 0 si `lambd < 0`.
+  """
   return - math.log(random()) / lambd
+
+def composition_method(
+  gens: List[Callable[[], float]],
+  probs: List[float]
+) -> float:
+  """Método de composición.
+
+  Método de composición para la generación de una variable aleatoria continua
+  `X` con función de distribución acumulada dada por `F(x) = p_1 * F_1(x) +
+  ... + p_n * F_n(x)`, con cada `F_i` función de distribución acumulada de
+  una variable aleatoria continua `X_i`, y cada `p_i` constante positiva tal
+  que `p_1 + ... + p_n = 1`.
+
+  Parametros
+  ----------
+  gens : List[() -> float]
+    Funciones generadoras de `X_1, ..., X_n`.
+  probs : List[float]
+    Probabilidades `p_1, ..., p_n`.
+
+  Retorna
+  -------
+  float
+    Un valor en el rango de `X`.
+  """
+  n = len(gens)
+  i = disc.inverse_trans_arr(probs, list(range(n)))
+  X_i = gens[i]
+  return X_i()
+
+def accept_reject_method(
+  Y_gen: Callable[[], float],
+  f: Callable[[float], float],
+  g: Callable[[float], float],
+  c: float
+) -> float:
+  """Método de aceptación y rechazo.
+
+  Método de aceptación y rechazo para la generación de una variable aleatoria
+  continua `X`.
+
+  Parámetros
+  ----------
+  Y_gen : () -> float
+    Función generadora de una variable aleatoria continua `Y`.
+  f : (float) -> float
+    Función de densidad de `X`.
+  g : (float) -> float
+    Función de densidad de `Y`.
+  c : float
+    Cota superior de `f(x) / g(x)` para `x` tal que `f(x) != 0`.
+
+  Retorna
+  -------
+  float
+    Un valor en el rango de `X`.
+  """
+  x = Y_gen()
+  while random() >= f(x) / (c * g(x)): x = Y_gen()
+  return x
--- src/p5.py
+++ src/p5.py
@@ -1,8 +1,11 @@
+from time import time
 from random import random
 from prettytable import PrettyTable
 import continuous as cont
 import simulate as sim
 import math
+
+sims = 10_000
 
 def ex1a():
   u = random()
@@ -33,7 +36,6 @@ def ex2_weibull(lambd, beta):
   return lambd * (-math.log(1-u)) ** (1/beta)
 
 def ex2():
-  sims = 10_000
   a, mu, k, lambd, beta = 2, 2, 2, 1, 2
 
   pareto = lambda: ex2_pareto(a)
@@ -61,7 +63,40 @@ def ex2():
   return
 
 
+def ex3b():
+  gens = [
+    lambda: cont.exponential(3),
+    lambda: cont.exponential(5),
+    lambda: cont.exponential(7)
+  ]
+  probs = [0.5, 0.3, 0.2]
+  return cont.composition_method(gens, probs)
+
+def ex3b_optimized():
+  u = random()
+  aux = -1 * math.log(random())
+  if u < 0.5: return 3 * aux
+  elif u < 0.8: return 5 * aux
+  else: return 7 * aux
+
 def ex3():
+  exp_val = 4.4 # pre-calculated
+
+  start = time()
+  exp_val_sim = sim.expected_value(sims, ex3b)
+  time_sim = time() - start
+
+  start = time()
+  exp_val_optimized = sim.expected_value(sims, ex3b_optimized)
+  time_optimized = time() - start
+
+  table = PrettyTable(['', 'E[X]', 'Tiempo (s)'])
+  table.align = 'l'
+  table.add_row(['Valor exacto', exp_val, '-'])
+  table.add_row([f'{sims} sims.', exp_val_sim, time_sim])
+  table.add_row([f'{sims} sims. (optimizado)', exp_val_optimized, time_optimized])
+
+  print(table)
   return
 
 
@@ -71,24 +106,151 @@ def ex4():
   return u ** (1/y)
 
 
-def ej5():
+def ex5_M(random_vars):
+  n = len(random_vars)
+  return max(random_vars[i]() for i in range(n))
+
+def ex5_m(random_vars):
+  n = len(random_vars)
+  return min(random_vars[i]() for i in range(n))
+
+def ex5():
+  n = 10
+
+  gens = [
+    lambda: cont.exponential(1),
+    lambda: cont.exponential(2),
+    lambda: cont.exponential(3)
+  ]
+
+  M_sample = [ex5_M(gens) for _ in range(n)]
+  m_sample = [ex5_m(gens) for _ in range(n)]
+
+  table = PrettyTable()
+  table.title = f'Muestra de tamaño {n}'
+  table.add_column('M', M_sample)
+  table.add_column('m', m_sample)
+  table.add_row([f'E[M] = {sum(M_sample) / n}', f'E[m] = {sum(m_sample) / n}'])
+
+  table_lines = table.get_string().split('\n')
+
+  result_lines = 1
+  print("\n".join(table_lines[:-(result_lines + 1)]))
+  print(table_lines[2])
+  print("\n".join(table_lines[-(result_lines + 1):]))
+
   return
 
 
-def ej6():
+def ex6_max(n):
+  return max(random() for _ in range(n))
+
+def ex6_acc_rej(n):
+  x = random()
+  while random() >= x**(n-1): x = random()
+  return x
+
+def ex6_inv_tr(n):
+  return random() ** (1/n)
+
+def ex6():
+  n = 10
+
+  start = time()
+  sample_max = [ex6_max(n) for _ in range(sims)]
+  time_max = time() - start
+
+  start = time()
+  sample_acc_rej = [ex6_acc_rej(n) for _ in range(sims)]
+  time_acc_rej = time() - start
+
+  start = time()
+  sample_inv_tr = [ex6_inv_tr(n) for _ in range(sims)]
+  time_inv_tr = time() - start
+
+  table = PrettyTable(['Método', 'E[X]', 'Tiempo (s)'])
+  table.align = 'l'
+  table.add_row(['Máximo', sum(sample_max) / sims, time_max])
+  table.add_row(['Aceptación y rechazo', sum(sample_acc_rej) / sims, time_acc_rej])
+  table.add_row(['Transformada inversa', sum(sample_inv_tr) / sims, time_inv_tr])
+
+  print(table)
   return
 
 
-def ej7():
+def ex7_inv_tr():
+  return math.exp(random())
+
+def ex7_acc_rej():
+  x = random() * (math.e - 1) + 1
+  while random() >= 1/x: x = random() * (math.e - 1) + 1
+  return x
+
+def ex7():
+  start = time()
+  sample_inv_tr = [ex7_inv_tr() for _ in range(sims)]
+  time_inv_tr = time() - start
+  exp_val_inv_tr = sum(sample_inv_tr) / sims
+  prob_success_inv_tr = sum(x <= 2 for x in sample_inv_tr) / sims
+
+  start = time()
+  sample_acc_rej = [ex7_acc_rej() for _ in range(sims)]
+  time_acc_rej = time() - start
+  exp_val_acc_rej = sum(sample_acc_rej) / sims
+  prob_success_acc_rej = sum(x <= 2 for x in sample_acc_rej) / sims
+
+  table = PrettyTable(['#', 'E[X]', 'P(X<=2)', 'Tiempo (s)'])
+  table.title = 'Ejercicio 7'
+  table.align = 'l'
+  table.add_row(['Valor exacto', math.e - 1, math.log(2 ), '-'])
+  table.add_row(['Transformada inversa', exp_val_inv_tr, prob_success_inv_tr, time_inv_tr])
+  table.add_row(['Aceptación y rechazo', exp_val_acc_rej, prob_success_acc_rej, time_acc_rej])
+
+  print(table)
   return
 
 
-def ej8():
+def ex8_inv_tr():
+  u = random()
+  if u < 0.5: return math.sqrt(2 * u)
+  else: return 2 - math.sqrt(2 - 2 * u)
+
+def ex8_acc_rej():
+  x = random() * 2
+  u = random()
+  while (x < 1 and u >= x) or u >= 2 - x:
+    x = random() * 2
+    u = random()
+  return x
+
+def ex8():
+  is_sucess = lambda X: X > 1.5
+
+  start = time()
+  sample_inv_tr = [ex8_inv_tr() for _ in range(sims)]
+  time_inv_tr = time() - start
+  exp_val_inv_tr = sum(sample_inv_tr) / sims
+  prob_success_inv_tr = sum(is_sucess(x) for x in sample_inv_tr) / sims
+
+  start = time()
+  sample_acc_rej = [ex8_acc_rej() for _ in range(sims)]
+  time_acc_rej = time() - start
+  exp_val_acc_rej = sum(sample_acc_rej) / sims
+  prob_success_acc_rej = sum(is_sucess(x) for x in sample_acc_rej) / sims
+
+  table = PrettyTable(['#', 'E[X]', 'P(X>=1.5)', 'Tiempo (s)'])
+  table.title = 'Ejercicio 8'
+  table.align = 'l'
+  table.add_row(['Valor exacto', 1, 0.125, '-'])
+  table.add_row(['Transformada inversa', exp_val_inv_tr, prob_success_inv_tr, time_inv_tr])
+  table.add_row(['Aceptación y rechazo', exp_val_acc_rej, prob_success_acc_rej, time_acc_rej])
+
+  print(table)
   return
 
 
 def main():
-  ex2()
+  ex8()
   return
 
 if __name__ == '__main__':
