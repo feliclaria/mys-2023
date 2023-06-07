d556e453aac85c7b67da06664a36f4718df1a8ee
feliclaria <felipe.claria@mi.unc.edu.ar>
2023-06-07 18:48:46 -0300

    p6 cleaning

--- src/estimate.py
+++ src/estimate.py
@@ -6,77 +6,47 @@ def mean(
   X: Callable[..., float],
   *X_args: ...
 ) -> float:
-  """Media muestral.
-
-  Parámetros
-  ----------
-  tol : float
-    Máxima tolerancia deseada para el estimador de la varianza muestral.
-  X : (...) -> float
-    Función generadora de una variable aleatoria.
-  *X_args : (...)
-    Argumentos con los que llamar a `X`.
-
-  Retorna
-  -------
-  float
-    Media de una muestra cuyo tamaño es tal que el estimador de su varianza
-    muestral no supera la tolerancia deseada.
-  """
-  n, E_prev, E, S_sqr = 0, 0, 0, 0
+  n = 1
+  mean = X(*X_args)
   tol_sqr = tol**2
+  mean_prev, S_sqr = 0, 0
 
-  while n < 100 or n < S_sqr / tol_sqr:
+  while n < 100 or n <= S_sqr / tol_sqr:
     n += 1
-    E_prev = E
-    E += (X(*X_args) - E_prev) / (n+1)
-    S_sqr = S_sqr * (n-1) / n + (n+1) * (E - E_prev)**2
+    mean_prev = mean
+    mean += (X(*X_args) - mean_prev) / n
+    S_sqr = S_sqr * (n-2) / (n-1) + n * (mean - mean_prev)**2
 
-  return E
+  return mean
 
-def mean_conf_int(
+def mean_confidence_int(
   z_alfa_2 : float,
   L : float,
   X : Callable[..., float],
   *X_args : ...
 ) -> float:
-  """Media muestral por intervalo de confianza.
-
-  Parámetros
-  ----------
-  z_alfa_2 : float
-    Valor crítico asociado al intervalo de confianza.
-  L : float
-    Máxima tolerancia deseada para la longitud del intervalo de confianza.
-  X : (...) -> float
-    Función generadora de una variable aleatoria.
-  *X_args : (...)
-    Argumentos con los que llamar a `X`.
-
-  Retorna
-  -------
-  float
-    Media de una muestra que pertenece a un intervalo de confianza
-    `(1-alfa)*100%` cuyo tamaño no supera 'L'.
-  """
   return mean(L / (2 * z_alfa_2), X, *X_args)
 
-def monte_carlo(
+def integral_0_to_1(
   tol: float,
-  h: Callable[[float], float]
+  g: Callable[[float], float]
 ) -> float:
-  """Monte carlo para la integral desde 0 a 1 de `h(x) dx`.
+  return mean(tol, lambda: g (1 - random()))
 
-  Parámetros
-  ----------
-  tol : float
-    Máxima tolerancia deseada para el estimador de la varianza muestral.
-  h :
-    Función a integrar.
+def integral_a_to_b(
+  tol: float,
+  g: Callable[[float], float],
+  a: float,
+  b: float
+) -> float:
+  h = lambda y: g(a + (b-a) * y)
+  X = lambda: h(1 - random())
+  return mean(tol / (b-a), X)
 
-  Retorna
-  -------
-  float
-    Integral desde 0 a 1 de `h(x) dx`
-  """
-  return mean(tol, h, 1 - random())
+def integral_0_to_inf(
+  tol: float,
+  g: Callable[[float], float]
+) -> float:
+  h = lambda y: 1 / y**2 * g((1-y) / y)
+  X = lambda: h(1 - random())
+  return mean(tol, X)
--- src/p6.py
+++ src/p6.py
@@ -2,119 +2,103 @@ from prettytable import PrettyTable
 from random import random
 from scipy.integrate import quad
 
+import numpy as np
 import continuous as cont
 import math
 
 
 def ex1():
-  n = 0
-  E_prev, E, S_sqr = 0, 0, 0
+  n = 1
+  mean = cont.normal(0, 1)
+  mean_prev, S_sqr = 0, 0
 
-  while n < 100 or n < S_sqr * 100:
+  while n < 100 or n <= S_sqr * 100:
     n += 1
-    E_prev = E
-    E += (cont.normal(0, 1) - E_prev) / (n+1)
-    S_sqr = S_sqr * (n-1) / n + (n+1) * (E - E_prev)**2
+    mean_prev = mean
+    mean += (cont.normal(0, 1) - mean_prev) / n
+    S_sqr = S_sqr * (n-2) / (n-1) + n * (mean - mean_prev)**2
 
-  table = PrettyTable(['Tamaño de la muestra', 'Media muestral', 'Varianza muestral'])
-  table.add_row([n, E, S_sqr])
+  table = PrettyTable(['Nro. sims.', 'Media muestral', 'Varianza muestral'])
+  table.title = 'Ejercicio 1'
+  table.add_row([n, mean, S_sqr])
   print(table)
 
 
-def ex2i():
-  g = lambda x: math.exp(x) / math.sqrt(2 * x)
+def ex2_monte_carlo(h):
+  n = 1
+  theta = h(1-random())
+  theta_prev, S_sqr = 0, 0
 
-  n = 0
-  theta_prev, theta, S_sqr = 0, 0, 0
-
-  while n < 100 or n < S_sqr * 10_000:
+  while n < 100 or n <= S_sqr * 10_000:
     n += 1
     theta_prev = theta
-    theta += (g(1-random()) - theta_prev) / (n+1)
-    S_sqr = S_sqr * (n-1) / n + (n+1) * (theta - theta_prev)**2
+    theta += (h(1-random()) - theta_prev) / n
+    S_sqr = S_sqr * (n-2) / (n-1) + n * (theta - theta_prev)**2
 
-  return quad(g, 0, 1)[0], theta, n
+  return n, theta
+
+def ex2i():
+  g = lambda x: math.exp(x) / math.sqrt(2 * x)
+  n, theta = ex2_monte_carlo(g)
+  value, _ = quad(g, 0, 1)
+  return n, theta, value
 
 def ex2ii():
   g = lambda x: x**2 * math.exp(-x**2)
   h = lambda y: 2 * g((1-y) / y) / (y**2)
-
-  n = 0
-  theta_prev, theta, S_sqr = 0, 0, 0
-
-  while n < 100 or n < S_sqr * 10_000:
-    n += 1
-    theta_prev = theta
-    theta += (h(1-random()) - theta_prev) / (n+1)
-    S_sqr = S_sqr * (n-1) / n + (n+1) * (theta - theta_prev)**2
-
-  return quad(h, 0, 1)[0], theta, n
+  n, theta = ex2_monte_carlo(h)
+  value, _ = quad(g, -np.inf, np.inf)
+  return n, theta, value
 
 def ex2():
-  val_i, theta_i, n_i = ex2i()
-  val_ii, theta_ii, n_ii = ex2ii()
-
-  table = PrettyTable(['', 'Integral (i)', 'Integral (ii)'])
-  table.add_row(['Aprox. numérica', val_i, val_ii])
-  table.add_row(['Monte carlo', theta_i, theta_ii])
-  table.add_row(['Nro. sims.', n_i, n_ii])
+  table = PrettyTable(['Integral', 'Nro. sims.', 'Media muestral', 'Valor aprox.'])
+  table.title = 'Ejercicio 2'
+  table.add_row(['(i)', *ex2i()])
+  table.add_row(['(ii)', *ex2ii()])
   print(table)
 
+
+def ex3_monte_carlo(h):
+  sims = [1_000, 5_000, 7_000]
+  results = []
+
+  n = 1
+  theta = h(1-random())
+  theta_prev, S_sqr = 0, 0
+
+  while n < 100 or n <= 3_841_600 * S_sqr:
+    if n in sims:
+      const = 1.96 * math.sqrt(S_sqr/n)
+      results.append((n, round(theta, 4), round(math.sqrt(S_sqr), 4),
+                      (round(theta - const, 4), round(theta + const, 4))
+                      ))
+    n += 1
+    theta_prev = theta
+    theta += (h(1-random()) - theta_prev) / n
+    S_sqr = S_sqr * (n-2) / (n-1) + n * (theta - theta_prev)**2
+
+  const = 1.96 * math.sqrt(S_sqr/n)
+  results.append((n, round(theta, 4), round(math.sqrt(S_sqr), 4),
+                  (round(theta - const, 4), round(theta + const, 4))
+                  ))
+  return results
 
 def ex3i():
   g = lambda x: math.sin(x) / x
   h = lambda y: math.pi * g(math.pi * (y+1))
 
-  sims = [1_000, 5_000, 7_000]
-  I_list = [sum(h(1-random()) for _ in range(n)) / n for n in sims]
-  S_sqr_list = [sum((h(1-random()) - I)**2 for _ in range(n)) / n for n, I in zip(sims, I_list)]
-  IC_list = [(I - 1.96 * math.sqrt(S_sqr/n), I + math.sqrt(S_sqr/n)) for n, I, S_sqr in zip(sims, I_list, S_sqr_list)]
-
-  results = list(zip(sims, I_list, S_sqr_list, IC_list))
-
-  n, theta_prev, theta, S_sqr = 0, 0, 0, 0
-  while n < 100 or n < 3_841_600 * S_sqr:
-    n += 1
-    theta_prev = theta
-    theta += (h(1-random()) - theta_prev) / (n+1)
-    S_sqr = S_sqr * (n-1) / n + (n+1) * (theta - theta_prev)**2
-  IC = theta - 1.96 * math.sqrt(S_sqr/n), theta + 1.96 * math.sqrt(S_sqr/n)
-
-  results.append((n, theta, S_sqr, IC))
-
   table = PrettyTable(['Nro. sims.', 'I', 'S', 'IC(95%)'])
   table.title = 'Ejercicio 3i'
-  for n, I, S_sqr, IC in results:
-    table.add_row([n, round(I, 4), round(math.sqrt(S_sqr), 4), tuple(map(lambda x: round(x, 4), IC))])
-
+  table.add_rows(ex3_monte_carlo(h))
   print(table)
 
 def ex3ii():
   g = lambda x: 3 / (3 + x**4)
   h = lambda y: 1 / y**2 * g((1-y) / y)
 
-  sims = [1_000, 5_000, 7_000]
-  I_list = [sum(h(1-random()) for _ in range(n)) / n for n in sims]
-  S_sqr_list = [sum((h(1-random()) - I)**2 for _ in range(n)) / n for n, I in zip(sims, I_list)]
-  IC_list = [(I - 1.96 * math.sqrt(S_sqr/n), I + math.sqrt(S_sqr/n)) for n, I, S_sqr in zip(sims, I_list, S_sqr_list)]
-
-  results = list(zip(sims, I_list, S_sqr_list, IC_list))
-
-  n, theta_prev, theta, S_sqr = 0, 0, 0, 0
-  while n < 100 or n < 3_841_600 * S_sqr:
-    n += 1
-    theta_prev = theta
-    theta += (h(1-random()) - theta_prev) / (n+1)
-    S_sqr = S_sqr * (n-1) / n + (n+1) * (theta - theta_prev)**2
-  IC = theta - 1.96 * math.sqrt(S_sqr/n), theta + 1.96 * math.sqrt(S_sqr/n)
-
-  results.append((n, theta, S_sqr, IC))
-
   table = PrettyTable(['Nro. sims.', 'I', 'S', 'IC(95%)'])
   table.title = 'Ejercicio 3ii'
-  for n, I, S_sqr, IC in results:
-    table.add_row([n, round(I, 4), round(math.sqrt(S_sqr), 4), tuple(map(lambda x: round(x, 4), IC))])
-
+  table.add_rows(ex3_monte_carlo(h))
   print(table)
 
 def ex3():
@@ -123,24 +107,38 @@ def ex3():
 
 
 def ex4_N():
-  N = 0
-  S = 0
+  N, S = 0, 0
   while S <= 1:
     N += 1
     S += random()
   return N
 
 def ex4():
-  n, e_prev, e, S_sqr = 0, 0, 0, 0
-  while n < 100 or n < 24_586.24 * S_sqr:
+  n = 1
+  e = ex4_N()
+  e_prev, S_sqr = 0, 0
+
+  results = []
+
+  while n < 100 or n <= 24_586.24 * S_sqr:
+    if n == 1_000:
+      const = 1.96 * math.sqrt(S_sqr/n)
+      results.append((n, round(e, 4), round(math.sqrt(S_sqr/n), 4),
+                      (round(e - const, 4), round(e + const, 4))
+                      ))
     n += 1
     e_prev = e
-    e += (ex4_N() - e_prev) / (n+1)
-    S_sqr = S_sqr * (n-1) / n + (n+1) * (e - e_prev)**2
+    e += (ex4_N() - e_prev) / n
+    S_sqr = S_sqr * (n-2) / (n-1) + n * (e - e_prev)**2
 
-  table = PrettyTable(['n', 'ê', 'S²(n)'])
+  const = 1.96 * math.sqrt(S_sqr/n)
+  results.append((n, round(e, 4), round(math.sqrt(S_sqr/n), 4),
+                  (round(e - const, 4), round(e + const, 4))
+                  ))
+
+  table = PrettyTable(['Nro. sims', 'e', 'S', 'IC(95%)'])
   table.title = 'Ejercicio 4'
-  table.add_row([n, e, S_sqr])
+  table.add_rows(results)
   print(table)
 
 
@@ -162,7 +160,7 @@ def ex6a():
   return n, round(pi, 4), round(s, 4), (round(pi - const, 4), round(pi + const, 4))
 
 def ex6b():
-  n, p_prev, p = 0, 0, 0
+  n, p, p_prev = 0, 0, 0
   while n < 100 or n <= 11333.12 * p * (1-p):
     n += 1
     p_prev = p
@@ -181,6 +179,10 @@ def ex6():
 
 
 def main():
+  ex1()
+  ex2()
+  ex3()
+  ex4()
   ex6()
 
 if __name__ == '__main__':
