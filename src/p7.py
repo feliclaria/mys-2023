9462c17f4b7deee75d20cfa0ad0a897f96c5ebc6
feliclaria <felipe.claria@mi.unc.edu.ar>
2023-06-14 00:35:15 -0300

    p7: even more bugfixes and refactors

--- src/p7.py
+++ src/p7.py
@@ -4,42 +4,54 @@ import scipy.stats as sp
 import numpy as np
 import p_value as pval
 
-sims = 10_000
+
+SIMS = 10_000
+
+def sample_counts(sample, pmf, support):
+  counts = Counter(sample)
+  freqs = np.array([counts.get(k, 0) for k in support])
+  probs = np.array([pmf(k) for k in support])
+  return probs, freqs
+
+def rvs_gen(X):
+  rvs = lambda size: np.array([X() for _ in range(size)])
+  return rvs
+
 
 def ex1():
   probs = np.array([0.25, 0.5, 0.25])
   freqs = np.array([141, 291, 132])
 
   p_value_chi2 = pval.pearson_chi2(probs, freqs)
-  p_value_sims = pval.pearson_simulate(sims, probs, freqs)
+  p_value_binom = pval.pearson_simulate(SIMS, probs, freqs)
 
-  table = PrettyTable(['', 'Pearson', f'{sims} sims.'])
+  table = PrettyTable(['', 'Chi cuadrado', f'{SIMS} sims.'])
   table.title = 'Ejercicio 1'
-  table.add_row(['p-valor', p_value_chi2, p_value_sims])
+  table.add_row(['p-valor', p_value_chi2, p_value_binom])
   print(table)
 
 
 def ex2():
-  probs = [1/6]*6
-  freqs = [158, 172, 164, 181, 160, 165]
+  probs = np.array([1/6]*6)
+  freqs = np.array([158, 172, 164, 181, 160, 165])
 
-  pearson = pval.pearson_chi2(probs, freqs)
-  sim = pval.pearson_simulate(sims, probs, freqs)
+  p_value_chi2 = pval.pearson_chi2(probs, freqs)
+  p_value_sims = pval.pearson_simulate(SIMS, probs, freqs)
 
-  table = PrettyTable(['', 'Pearson', f'{sims} sims.'])
+  table = PrettyTable(['', 'Chi cuadrado', f'{SIMS} sims.'])
   table.title = 'Ejercicio 2'
-  table.add_row(['p-valor', pearson, sim])
+  table.add_row(['p-valor', p_value_chi2, p_value_sims])
   print(table)
 
 
 def ex3():
   sample = [0.12, 0.18, 0.06, 0.33, 0.72, 0.83, 0.36, 0.27, 0.77, 0.74]
   cdf = sp.uniform.cdf
-  p_value = pval.kolmogorov_smirnov_simulate(sims, sample, cdf)
+  p_value_sims = pval.kolmogorov_smirnov_simulate(SIMS, sample, cdf)
 
   table = PrettyTable(['Nro. sims.', 'p-valor'])
   table.title = 'Ejercicio 3'
-  table.add_row([sims, p_value])
+  table.add_row([SIMS, p_value_sims])
   print(table)
 
 
@@ -49,13 +61,33 @@ def ex4():
     122.0, 8.0, 146.0, 33.0, 41.0, 99.0
   ]
   cdf = lambda x: sp.expon.cdf(x, scale=50)
-  p_value = pval.kolmogorov_smirnov_simulate(sims, sample, cdf)
+  p_value_sims = pval.kolmogorov_smirnov_simulate(SIMS, sample, cdf)
 
   table = PrettyTable(['Nro. sims.', 'p-valor'])
   table.title = 'Ejercicio 4'
-  table.add_row([sims, p_value])
+  table.add_row([SIMS, p_value_sims])
   print(table)
 
+
+def ex5_chi2(probs, freqs):
+  return pval.pearson_chi2(probs, freqs, params=1, digits=10)
+
+def ex5_sims(sims, probs, freqs, dist, support):
+  size = np.sum(freqs)
+  t = pval.pearson_statistic(probs, freqs)
+
+  successes = 0
+  for _ in range(sims):
+    sample_sim = dist.rvs(size)
+
+    p_sim = np.mean(sample_sim) / 8
+    dist_sim = sp.binom(8, p_sim)
+
+    probs_sim, freqs_sim = sample_counts(sample_sim, dist_sim.pmf, support)
+    t_sim = pval.pearson_statistic(probs_sim, freqs_sim)
+    successes += t <= t_sim
+
+  return successes / sims
 
 def ex5():
   sample = np.array([
@@ -67,50 +99,22 @@ def ex5():
   p =  np.mean(sample) / n
   dist = sp.binom(n, p)
 
-  bins = range(n+2)
-  values = range(n+1)
-  pmf = lambda x, _: dist.pmf(x)
+  support = range(n+1)
+  counts = Counter(sample)
 
-  probs_chi2, freqs_chi2 = pval.sample_counts(sample, bins, values, pmf)
-  p_value_chi2 = pval.pearson_chi2(probs_chi2, freqs_chi2, params=1)
+  probs = np.array([dist.pmf(k) for k in support])
+  freqs = np.array([counts.get(k, 0) for k in support])
 
-  def pmf_sim(k, sample_sim):
-    n = 8
-    p = np.mean(sample_sim) / n
-    return sp.binom(n, p).pmf(k)
+  p_value_chi2 = ex5_chi2(probs, freqs)
+  p_value_sims = ex5_sims(SIMS, probs, freqs, dist, support)
 
-  probs_sims, freqs_sims = pval.sample_counts(sample, bins, values, pmf)
-  p_value_sims = pval.pearson_simulate(sims, probs_sims, freqs_sims,
-                                       bins, values, dist.rvs, pmf_sim)
-
-  table = PrettyTable(['', 'Pearson', f'{sims} sims.'])
+  table = PrettyTable(['', 'Pearson', f'{SIMS} sims.'])
   table.title = 'Ejercicio 5'
   table.add_row(['p-valor', p_value_chi2, p_value_sims])
   print(table)
 
 
-
-
-def ex5_sims():
-  sample = [6, 7, 3, 4, 7, 3, 7, 2, 6, 3, 7, 8, 2, 1, 3, 5, 8, 7]
-
-  mean = sum(sample) / len(sample)
-
-  n = 8
-  p = mean / n
-  binom_dist = sp.binom(n, p)
-
-  values = range(n+1)
-  probs, freqs = pval.sample_counts(list(sample), values, binom_dist.pmf)
-
-  return pval.pearson_simulate(sims, probs, freqs, values, binom_dist)
-
-
-def si(*params):
-  print(params)
-
 def main():
-
   ex5()
 
 if __name__ == '__main__':
--- src/p_value.py
+++ src/p_value.py
@@ -1,12 +1,6 @@
 import numpy as np
-from collections import Counter
-from scipy.stats import uniform, chi2
+from scipy.stats import uniform, chi2, binom
 
-def sample_counts(sample, bins, values, pmf):
-  freqs, _ = np.histogram(sample, bins)
-  probs = np.array([pmf(k, sample) for k in values])
-
-  return probs, freqs
 
 def pearson_statistic(probs, freqs):
   size = np.sum(freqs)
@@ -14,23 +8,29 @@ def pearson_statistic(probs, freqs):
 
 def pearson_chi2(probs, freqs, params=0, digits=4):
   k = len(freqs)
-  T = pearson_statistic(probs, freqs)
-  p_val = 1 - chi2.cdf(T, k-params-1)
+  t = pearson_statistic(probs, freqs)
+  p_val = 1 - chi2.cdf(t, k-params-1)
   return round(p_val, digits)
 
-def pearson_simulate(sims, probs, freqs, bins, values, rvs, pmf, digits=4):
+def pearson_simulate(sims, probs, freqs, digits=4):
+  k = len(probs)
   size = np.sum(freqs)
   t = pearson_statistic(probs, freqs)
 
   successes = 0
   for _ in range(sims):
-    sample_sim = rvs(size)
-    probs_sim, freqs_sim = sample_counts(sample_sim, bins, values, pmf)
-    t_sim = pearson_statistic(probs_sim, freqs_sim)
+    freqs_sim = np.zeros(k, dtype=int)
+    for j in range(k):
+      n = size - np.sum(freqs_sim)
+      p = probs[j] / (1 - np.sum(probs[:j]))
+      freqs_sim[j] = binom(n=n, p=p).rvs()
+
+    t_sim = pearson_statistic(probs, freqs_sim)
     successes += t <= t_sim
   p_val = successes / sims
 
   return round(p_val, digits)
+
 
 def kolmogorov_smirnov_statistic(sample, cdf):
   size = len(sample)
